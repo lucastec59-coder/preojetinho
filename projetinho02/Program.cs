520d3de409b91a723bb577b516e24c04f86d771d
Lucas Mendes <Lucas Mendes@DESKTOP-SRUQIEA>
2025-11-08 14:35:02 -0300

    Criei um novo projeto e adicionei um novo campo para digitar o nome

--- projetinho02/Program.cs
+++ projetinho02/Program.cs
@@ -1,6 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("hello, Lucas Mendes");
-
-
-
 Console.WriteLine("digite seu nome :");
+
+Console.WriteLine("Digita ai po:")
